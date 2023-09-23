dbfe526d65644a1613572dae601d8c18e7fa70b9
m4zin <mazin.sherif@gmail.com>
2023-09-23 10:45:36 +0400

    addition of functional input boxes with error checking

--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,56 @@
-let drawingBoard = document.querySelector(".drawing-board");
-let userInput = Number(prompt("Enter grid size: "))
-let side = userInput
+function addSquaresInContainer() {
+    let side = input.value
 
-// Making sure the size of grid is higher than 0 and less than 16
-if (userInput > 0 && userInput <= 64) 
-{
-    let areaOfContainerSquare = side * side;
-    let areaOfInsideSquare = 400 / userInput;
+    if(side > 0 && side <= 64) 
+    {
+        let areaOfContainerSquare = side * side
+        let areaOfInsideSquare = 400 / side;
 
-    for (let i = 0; i < areaOfContainerSquare; i++) {
+        //to clear previous error display msg.
+        error.innerHTML = ''
 
-        let square = document.createElement("div");
+        //to clear the previous grid.
+        if(drawingBoard.hasChildNodes()) {
+            drawingBoard.innerHTML = ''
+        }
 
-        square.classList.add("square");
-        square.style.border = "0.1px solid black";
-        square.style.height = `${areaOfInsideSquare}px`;
-        square.style.width = `${areaOfInsideSquare}px`;
-        drawingBoard.appendChild(square);
+        //create new squares inside containre based on user input grid size.
+        for (let i = 0; i < areaOfContainerSquare; i++) {
+            let square = document.createElement("div");
 
-        square.addEventListener(
-            "mouseenter",
-            (event) => {
-                let colorPicked = document.getElementById("favColor").value;
-                event.target.style.backgroundColor = `${colorPicked}`;
-            },
-        false
-        );
+            square.classList.add("square");
+            square.style.border = "0.1px solid black";
+            square.style.height = `${areaOfInsideSquare}px`;
+            square.style.width = `${areaOfInsideSquare}px`;
+            drawingBoard.appendChild(square);
+
+            //when mouse is hovered over grid.
+            square.addEventListener(
+                "mouseenter",
+                (event) => {
+                    let colorPicked = document.getElementById("favColor").value;
+                    event.target.style.backgroundColor = `${colorPicked}`;
+                },
+            false
+            );
+        }
     }
-} else {
-    alert("Invalid input!")
+    else {
+        error.textContent = 'Invalid input!'
+        error.style.color = 'red'
+    } 
+}
+
+function reloadPage() {
     window.location.reload()
 }
+
+const button = document.querySelector('.size-btn')
+const drawingBoard = document.querySelector('.drawing-board')
+const input = document.querySelector('#size')
+const error = document.querySelector('.error-log')
+const reset = document.querySelector('.reset-button')
+
+button.addEventListener('click', addSquaresInContainer)
+reset.addEventListener('click', reloadPage)
 
